c5a4f0fdfb3e37bb8430bc262c37689763dcfaa8
Kazi Anwar <kazisameen2702@gmail.com>
2023-12-21 18:14:23 -0500

    greedyTSP

--- GreedyTSP.hpp
+++ GreedyTSP.hpp
@@ -1,47 +1,41 @@
-// #ifndef GREEDY_TSP_HPP
-// #define GREEDY_TSP_HPP
+#ifndef GREEDY_TSP_HPP
+#define GREEDY_TSP_HPP
 
-// #include <vector>
-// #include <string>
+#include <vector>
+#include <string>
 
-// class GreedyTSP {
-// public:
-//     GreedyTSP(const std::string& filename);
+class GreedyTSP 
+{
+public:
+    GreedyTSP(const std::string& filename);
+    void greedyTSP();
+private:
+    struct Node 
+    {
+        int id;
+        double x, y; //city coords
+        bool visited;
+        std::vector<Node*> edges;
+    };
 
-//     // Function to perform the Greedy TSP algorithm
-//     void greedyTSP();
+    struct Edge 
+    {
+        Node* from;
+        Node* to;
+        double weight;
+    };
 
-// private:
-//     // Node structure to represent cities
-//     struct Node {
-//         int id;
-//         double x, y; // Coordinates of the city
-//         bool visited;
-//         std::vector<Node*> edges; // Nodes connected by edges
-//     };
+    double calculateDistance(const Node& city1, const Node& city2);
 
-//     // Edge structure to represent connections between cities
-//     struct Edge {
-//         Node* from;
-//         Node* to;
-//         double weight;
-//     };
+    void addEdgeToTour(Edge& edge);
 
-//     // Function to calculate the distance between two cities
-//     double calculateDistance(const Node& city1, const Node& city2);
+    bool createsCycle(Edge& edge);
 
-//     // Function to add an edge to the tour
-//     void addEdgeToTour(Edge& edge);
+    void printTour();
 
-//     // Function to check if adding an edge creates a cycle
-//     bool createsCycle(Edge& edge);
+    std::vector<Node> cities; 
+    std::vector<Edge> edges;  
+    double totalDistance;   
+};
 
-//     // Function to print the tour details
-//     void printTour();
-
-//     std::vector<Node> cities; // Vector to store information about cities
-//     std::vector<Edge> edges;  // Vector to store information about edges
-//     double totalDistance;     // Total distance traveled
-// };
-
-// #endif // GREEDY_TSP_HPP
+#endif 
